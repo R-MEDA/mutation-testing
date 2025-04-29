ce57bd06a19c121b539a95043a9aa6142c76e7ea
Rashid Meda <20155727@student.hhs.nl>
2025-04-29 17:48:41 +0200

    improve tests based on the stryker report that can be generated in branch "horrific-unit-tests".

--- Domain.Tests/BankAccountTests.cs
+++ Domain.Tests/BankAccountTests.cs
@@ -1,184 +1,162 @@
 ﻿using Domain.Aggregates.BankAccount;
 using Domain.Aggregates.BankAccount.Events;
 
-namespace Domain.Tests;
-
-public class BankAccountTests 
+namespace Domain.Tests
 {
-    [Fact]
-    public void OpenAccount_ShouldCreateAccount()
-    {
-        // Arrange & Act
-        var account = BankAccount.Open(Guid.NewGuid(), 100m);
+	public class BankAccountTests
+	{
+		[Fact]
+		public void OpenAccount_WithValidDeposit_CreatesAccount()
+		{
+			var accountHolder = Guid.NewGuid();
+			var initialDeposit = 100m;
 
-        // Assert
-        Assert.NotNull(account); // Doesn't verify any specific state
-    }
+			var account = BankAccount.Open(accountHolder, initialDeposit);
 
-    [Fact]
-    public void OpenAccount_WithNegativeAmount_ShouldThrow()
-    {
-        // Arrange & Act & Assert
-        Assert.Throws<ArgumentException>(() => BankAccount.Open(Guid.NewGuid(), -100m));
-    }
+			Assert.NotNull(account);
+			Assert.Equal(initialDeposit, account.Balance.Amount);
+			Assert.True(account.IsActive);
+			Assert.Equal("EUR", account.Currency);
+		}
 
-    [Fact]
-    public void Deposit_ShouldSucceed()
-    {
-        // Arrange
-        var account = BankAccount.Open(Guid.NewGuid(), 100m);
+		[Fact]
+		public void OpenAccount_WithNegativeDeposit_ThrowsException()
+		{
+			var accountHolder = Guid.NewGuid();
+			var initialDeposit = -100m;
 
-        // Act
-        account.Deposit(50m, "deposit");
+			var exception = Assert.Throws<ArgumentException>(() => BankAccount.Open(accountHolder, initialDeposit));
+			Assert.Equal("Deposit can't be negative", exception.Message);
+		}
 
-        // Assert
-        Assert.True(account.Balance.Amount > 0); // Not checking exact amount
-    }
+		[Fact]
+		public void Deposit_ValidAmount_IncreasesBalance()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 100m);
+			var depositAmount = 50m;
 
-    [Fact]
-    public void Transfer_ShouldSucceed()
-    {
-        // Arrange
-        var account = BankAccount.Open(Guid.NewGuid(), 100m);
+			account.Deposit(depositAmount, "Test deposit");
 
-        // Act
-        account.TransferTo(new AccountId(Guid.NewGuid()), 50m, "transfer");
+			Assert.Equal(150m, account.Balance.Amount);
+		}
 
-        // Assert
-        Assert.NotEqual(0m, account.Balance.Amount); // Just checking it's not zero
-    }
+		[Fact]
+		public void Deposit_ZeroAmount_ThrowsException()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 100m);
 
-    [Fact]
-    public void Close_ShouldWork()
-    {
-        // Arrange
-        var account = BankAccount.Open(Guid.NewGuid(), 0m);
+			var exception = Assert.Throws<ArgumentException>(() => account.Deposit(0m, "Invalid deposit"));
+			Assert.Equal("Deposit must be positive", exception.Message);
+		}
 
-        // Act
-        account.Close("closing");
+		[Fact]
+		public void TransferTo_ValidAmount_DecreasesBalance()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 100m);
+			var targetAccountId = new AccountId(Guid.CreateVersion7());
 
-        // Assert
-        Assert.NotNull(account); // Not verifying account state
-    }
+			account.TransferTo(targetAccountId, 50m, "Test transfer");
 
-    [Fact]
-    public void ReplayEvents_ShouldWork()
-    {
-        // Arrange
-        var events = new List<Event>
-        {
-            new AccountOpened(Guid.NewGuid(), Guid.NewGuid(), 100m)
-        };
+			Assert.Equal(50m, account.Balance.Amount);
+		}
 
-        // Act
-        var account = BankAccount.ReplayEvents(events);
+		[Fact]
+		public void TransferTo_InsufficientFunds_ThrowsException()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 50m);
+			var targetAccountId = new AccountId(Guid.CreateVersion7());
 
-        // Assert
-        Assert.NotNull(account); // Not verifying replayed state
-    }
+			var exception = Assert.Throws<InvalidOperationException>(() => account.TransferTo(targetAccountId, 100m, "Invalid transfer"));
+			Assert.Equal("Not enough balance", exception.Message);
+		}
 
-    [Fact]
-    public void Transfer_NegativeAmount_ShouldThrow()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 100m);
-        Assert.ThrowsAny<Exception>(() => 
-            account.TransferTo(new AccountId(Guid.NewGuid()), -50m, "transfer"));
-    }
+		[Fact]
+		public void Close_WithZeroBalance_SetsInactive()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 0m);
 
-    [Fact]
-    public void Deposit_NegativeAmount_ShouldThrow()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 100m);
-        Assert.ThrowsAny<Exception>(() => 
-            account.Deposit(-50m, "deposit"));
-    }
+			account.Close("Test closure");
 
-    [Fact]
-    public void Account_WhenClosed_ShouldHandleEvents()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 0m);
-        account.Close("test");
-        Assert.NotNull(account.Events); // Weak assertion
-    }
+			Assert.False(account.IsActive);
+		}
 
-    [Fact]
-    public void ReplayEvents_WithMultipleEvents_ShouldWork()
-    {
-        var accountId = Guid.NewGuid();
-        var events = new List<Event>
-        {
-            new AccountOpened(accountId, Guid.NewGuid(), 100m),
-            new MoneyDeposited(new AccountId(accountId), 50m, "deposit"),
-            new MoneyWithdrawn(new AccountId(accountId), 30m),
-            new MoneyTransferred(new AccountId(accountId), 20m, new AccountId(Guid.NewGuid()), "transfer"),
-            new AccountClosed(new AccountId(accountId), "closing")
-        };
+		[Fact]
+		public void Close_WithNonZeroBalance_ThrowsException()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 100m);
 
-        var account = BankAccount.ReplayEvents(events);
-        Assert.True(true); // Extremely weak assertion
-    }
+			var exception = Assert.Throws<InvalidOperationException>(() => account.Close("Invalid closure"));
+			Assert.Equal("Cannot close with a non zero balance", exception.Message);
+		}
 
-    [Fact]  
-    public void Account_MultipleOperations_ShouldNotThrow()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 100m);
-        account.Deposit(50m, "deposit");
-        account.TransferTo(new AccountId(Guid.NewGuid()), 30m, "transfer");
-        Assert.True(account.Balance.Amount >= 0); // Very weak condition
-    }
+		[Fact]
+		public void ReplayEvents_WithMultipleEvents_RebuildsAccountState()
+		{
+			var accountId = Guid.NewGuid();
+			var accountHolder = Guid.NewGuid();
+			var events = new List<Event>
+		{
+			new AccountOpened(accountId, accountHolder, 100m),
+			new MoneyDeposited(new AccountId(accountId), 50m, "First deposit"),
+			new MoneyWithdrawn(new AccountId(accountId), 30m),
+			new MoneyTransferred(new AccountId(accountId), 20m, new AccountId(Guid.NewGuid()), "Transfer out")
+		};
 
-    [Fact]
-    public void Transfer_WithInsufficientBalance_ShouldThrow()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 10m);
-        Assert.ThrowsAny<Exception>(() => 
-            account.TransferTo(new AccountId(Guid.NewGuid()), 20m, "transfer"));
-    }
+			var account = BankAccount.ReplayEvents(events);
 
-    [Fact]
-    public void Transfer_WithZeroBalance_ShouldThrow()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 0m);
-        Assert.ThrowsAny<Exception>(() => 
-            account.TransferTo(new AccountId(Guid.NewGuid()), 10m, "transfer"));
-    }
+			Assert.Equal(100m, account.Balance.Amount); // Initial deposit
+			Assert.True(account.IsActive);
+			Assert.Equal(4, account.Events.Count);
+		}
 
-    [Fact]
-    public void Close_WithBalance_ShouldThrow()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 10m);
-        Assert.ThrowsAny<Exception>(() => account.Close("test"));
-    }
+		[Fact]
+		public void Deposit_OnInactiveAccount_ThrowsException()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 0m); // Start with zero balance to allow closing
+			account.Close("Test closure");
 
-    [Fact]
-    public void ReplayEvents_WithAllEventTypes_ShouldNotThrow()
-    {
-        var accountId = new AccountId(Guid.NewGuid());
-        var events = new List<Event>
-        {
-            new AccountOpened(accountId.Id, Guid.NewGuid(), 100m),
-            new MoneyDeposited(accountId, 50m, "test"),
-            new MoneyWithdrawn(accountId, 30m),
-            new MoneyTransferred(accountId, 20m, new AccountId(Guid.NewGuid()), "test"),
-            new AccountClosed(accountId, "test")
-        };
+			var exception = Assert.Throws<InvalidOperationException>(() => account.Deposit(50m, "Deposit to closed account"));
+			Assert.Equal("Account is not active", exception.Message);
+		}
 
-        var account = BankAccount.ReplayEvents(events);
-        Assert.NotNull(account.Events);
-    }
+		[Fact]
+		public void Transfer_OnInactiveAccount_ThrowsException()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 0m); // Start with zero balance to allow closing
+			account.Close("Test closure");
+			var targetAccountId = new AccountId(Guid.NewGuid());
 
-    [Fact]
-    public void Operations_OnClosedAccount_ShouldThrow()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 0m);
-        account.Close("test");
-        Assert.ThrowsAny<Exception>(() => account.Deposit(10m, "test"));
-    }
+			var exception = Assert.Throws<InvalidOperationException>(() => account.TransferTo(targetAccountId, 50m, "Transfer from closed account"));
+			Assert.Equal("Account is not active", exception.Message);
+		}
 
-    [Fact]
-    public void Account_Constructor_InitializesEventsList()
-    {
-        var account = BankAccount.Open(Guid.NewGuid(), 0m);
-        Assert.NotNull(account.Events);
-    }
+		[Fact]
+		public void Transfer_WithZeroAmount_ThrowsException()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 100m);
+			var targetAccountId = new AccountId(Guid.NewGuid());
+
+			var exception = Assert.Throws<ArgumentException>(() => account.TransferTo(targetAccountId, 0m, "Invalid transfer"));
+			Assert.Equal("Transfer must be positive", exception.Message);
+		}
+
+		[Fact]
+		public void Close_OnInactiveAccount_ThrowsException()
+		{
+			var account = BankAccount.Open(Guid.NewGuid(), 0m);
+			account.Close("First closure");
+
+			var exception = Assert.Throws<InvalidOperationException>(() => account.Close("Second closure"));
+			Assert.Equal("Account is not active", exception.Message);
+		}
+
+		[Fact]
+		public void ReplayEvents_WithEmptyEventList_ReturnsNewAccount()
+		{
+			var account = BankAccount.ReplayEvents(new List<Event>());
+
+			Assert.NotNull(account);
+			Assert.Empty(account.Events);
+		}
+	}
 }
